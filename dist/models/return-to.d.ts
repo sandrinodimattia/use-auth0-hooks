b4dfa373fd95b3ede66f8c5c82051fa0c1e8663a
Sandrino Di Mattia <contact@sandrino.be>
2020-05-11 15:54:13 +0200

    Removed dist folder

--- dist/models/return-to.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ReturnTo {
-    pathname: string | null;
-    query: object | null;
-}
-export interface ReturnToAppState {
-    returnTo?: ReturnTo | null;
-}
--- dist/utils/auth0.d.ts
+++
@@ -1,6 +0,0 @@
-import { Auth0Client } from 'context/auth0-context';
-import { ITokenResponse } from 'context/access-token-context';
-export declare function ensureClient(client: Auth0Client | null | undefined): Auth0Client;
-export declare const DEFAULT_SCOPE = "openid profile email";
-export declare function getUniqueScopes(...scopes: string[]): string;
-export declare function getTokenFromCache(client: Auth0Client, audience: string, scope: string): ITokenResponse | undefined;
